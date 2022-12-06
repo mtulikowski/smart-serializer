911e1418d2765746e043b55bb4adf7b0394454eb
mtulikowski <mariusz@tulikowski.eu>
2022-12-06 09:06:53 +0100

    Update Snapshot annotation

--- src/Annotations/Snapshot.php
+++ src/Annotations/Snapshot.php
@@ -17,32 +17,5 @@ final class Snapshot
     public bool $isRoute = false;
     public bool $isCollection = false;
     public bool $translate = false;
-//
-//    /**
-//     * @param bool $isObject
-//     * @param bool $isDate
-//     * @param string $dateFormat
-//     * @param string $fieldName
-//     * @param bool $isRoute
-//     * @param bool $isCollection
-//     * @param bool $translate
-//     */
-//    public function __construct(
-//        bool $isObject = false,
-//        bool $isDate = false,
-//        string $dateFormat = 'Y-m-d H:i:s',
-//        string $fieldName = '',
-//        bool $isRoute = false,
-//        bool $isCollection = false,
-//        bool $translate = false
-//    ) {
-//        $this->isObject = $isObject;
-//        $this->isDate = $isDate;
-//        $this->dateFormat = $dateFormat;
-//        $this->fieldName = $fieldName;
-//        $this->isRoute = $isRoute;
-//        $this->isCollection = $isCollection;
-//        $this->translate = $translate;
-//    }
 
 }
